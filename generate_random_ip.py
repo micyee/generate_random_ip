c011cca08514b8f1819158b50291d50fbd8f42ea
qrpengx <qrpengx@126.com>
2015-12-04 11:50:24 +0800

    add Readme.md

--- generate_random_ip.py
+++ generate_random_ip.py
@@ -6,7 +6,7 @@ import simplejson
 for x in xrange(1,10):
 	ip=random.choice(['58.56.','60.209.','110.96.','101.104.','101.236.','101.244.','106.52.','112.124.','123.64.','175.48.','175.64.','221.216.','42.208.','117.112.'])
 	ip += ".".join(map(str, (random.randint(0, 255)
-							for _ in range(2))))
+		for _ in range(2))))
 	url = 'http://ip.taobao.com/service/getIpInfo.php?ip=%s' % ip
 	f = urllib.urlopen(url).read()
 	s = simplejson.loads(f) 
