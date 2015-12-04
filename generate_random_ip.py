99081ef0e9c9acbfe6656603033f51eec0225fa7
qrpengx <qrpengx@126.com>
2015-12-04 11:41:15 +0800

    first commit

---
+++ generate_random_ip.py
@@ -0,0 +1,13 @@
+import random
+import urllib
+import re
+import simplejson
+for x in xrange(1,10):
+	ip=random.choice(['58.56.','60.209.','110.96.','101.104.','101.236.','101.244.','106.52.','112.124.','123.64.','175.48.','175.64.','221.216.','42.208.','117.112.'])
+	ip += ".".join(map(str, (random.randint(0, 255)
+							for _ in range(2))))
+	url = 'http://ip.taobao.com/service/getIpInfo.php?ip=%s' % ip
+	f = urllib.urlopen(url).read()
+	s = simplejson.loads(f) 
+	#print ip+": "+s['data']['country']+s['data']['area']+s['data']['region']+s['data']['city']+s['data']['isp']
+	print ip
